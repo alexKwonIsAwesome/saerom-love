5e5291828edfbf1e8e15df9294eda17a47966299
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2017-09-19 18:56:16 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<div>example</div>, document.getElementById("root"));
+registerServiceWorker();
